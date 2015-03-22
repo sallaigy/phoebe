660aab11a331e8be914c541f92097c3d9f6c694f
verboczy <verbocika@gmail.com>
2015-03-22 20:21:36 +0100

    Player osztaly

--- src/phoebe/GlueStain.java
+++ src/phoebe/GlueStain.java
@@ -1,7 +1,7 @@
 package phoebe;
 
 public class GlueStain extends Stain {
-
+	
 	@Override
 	public void interact(Player player) {
         Logger.methodEntry(this);
--- src/phoebe/Player.java
+++ src/phoebe/Player.java
@@ -14,42 +14,154 @@ public class Player {
     
     private Cell currentCell;
     
+    private static OilStain oilKey = new OilStain();
+    private static GlueStain glueKey = new GlueStain();
+    
+    /*
+     * Player konstruktora, amely egy cellát kap.
+     * Az aktuális cellát átállítja a kapott cellára.
+     * Létrehozza a HashMap-et, amiben tároljuk, hogy melyik foltból mennyi van.
+     */
+    public Player(Cell cell) {
+    	currentCell = cell;
+    	
+    	storedStains.put(oilKey, 3);
+    	
+    	storedStains.put(glueKey, 3);
+    }
+    
+    /*
+     * Visszaadja a kezdõpozíciót.
+     */
+    public Cell getInitialPosition() {
+    	Logger.methodEntry(this);
+    	
+    	Logger.methodExit(this);
+    	
+    	return initialPosition;
+    }
+    
+    /*
+     * Kezdõpozíció beállítása.
+     */
+    public void setInitialPosition(Cell init) {
+    	Logger.methodEntry(this, init.toString());
+    	
+    	this.initialPosition = init;
+    	
+    	Logger.methodExit(this);
+    }
+    
+    /*
+     * Visszatér azzal a cellával, amin éppen áll a játékos.
+     */
+    public Cell getCurrentCell() {
+    	Logger.methodEntry(this);
+    	
+    	Logger.methodExit(this);
+    	
+    	return currentCell;
+    }
+    
+    /*
+     * Átlépteti a játékost a paraméterként átvett cellára.
+     */
     public void move(Cell cell) {
         Logger.methodEntry(this, cell.toString());
+        
+        currentCell = cell;
+        
+        currentCell.setPlayer(this);
                 
         Logger.methodExit(this);
     }
     
+    /*
+     * Olaj- vagy ragacsfolt lerakása.
+     */
     public void putStain(String stainType) {
-        Logger.methodEntry(this, stainType);
-                
+        Logger.methodEntry(this, stainType);        
+        
+        if(stainType.equals("glue")) {
+        	GlueStain glueStain = new GlueStain();
+        	
+        	getCurrentCell().setGameObject(glueStain);
+        }
+        
+        if(stainType.equals("oil")) {
+        	OilStain oilStain = new OilStain();
+        	
+        	getCurrentCell().setGameObject(oilStain);
+        }                 
+       
         Logger.methodExit(this);        
     }
     
+    /*
+     * Alaphelyzetbe állítja a foltkészletet.
+     */
     public void resetStainCount() {
         Logger.methodEntry(this);
+        
+        storedStains.replace(oilKey, 3);
+        
+        storedStains.replace(glueKey, 3);
                 
         Logger.methodExit(this);        
     }
     
+    /*
+     * Meghívja annak a cellának az interact() metódusát, amelyiken tartózkodik.
+     */
     public void onTurnStart() {
         Logger.methodEntry(this);
+        
+        getCurrentCell().interact();
                 
         Logger.methodExit(this);        
     }
     
+    /*
+     * A kör végén visszaállítja a mozgathatóságot, és a sebességet.
+     * (Eltávolítja az elõzõ körben felvett foltok hatását.)
+     */
     public void onTurnEnd() {
         Logger.methodEntry(this);
+        
+        //alaphelyzetbe állítja a sebességet
+        this.setSpeed(2);
+        
+        //alaphelyzetbe állítja a mozgathatóságát
+        this.setCanChangeDirection(true);
                 
         Logger.methodExit(this);        
     }
     
+    /*
+     * Megadja a kezdõponttól megtett távolságot.
+     */
     public int getDistance() {
+    	Logger.methodEntry(this);
+    	
+    	Logger.methodExit(this); 
+    	
         return 0;
     }
     
+    /*
+     * A játék újraindítása.
+     * Alaphelyzetbe állítja a foltkészletet.
+     * Engedélyezi az irányváltoztatását.
+     * Alapértékre állítja a sebességet.
+     */
     public void reset() {
         Logger.methodEntry(this);
+        
+        this.resetStainCount();
+        
+        this.setCanChangeDirection(true);
+        
+        this.setSpeed(2);
                 
         Logger.methodExit(this);        
     }
@@ -59,20 +171,48 @@ public class Player {
         return super.toString();
     }
 
+    /*
+     * Visszatér azzal, hogy a játékos mozgathatja-e a robot.
+     */
     public boolean isCanChangeDirection() {
+    	Logger.methodEntry(this);
+    	
+    	Logger.methodExit(this);
+    	
         return canChangeDirection;
     }
 
+    /*
+     * Azt állítja be, hogy az adott játékos tudja-e irányítani a robotot.
+     */
     public void setCanChangeDirection(boolean canChangeDirection) {
-        this.canChangeDirection = canChangeDirection;
+    	Logger.methodEntry(this);
+    	
+    	this.canChangeDirection = canChangeDirection;
+        
+        Logger.methodExit(this);
     }
 
+    /*
+     * Visszatér a játékos sebességével.
+     */
     public int getSpeed() {
+    	Logger.methodEntry(this);
+    	
+    	Logger.methodExit(this);
+    	
         return speed;
     }
 
+    /*
+     * A paraméterként kapott értéket beállítja a játékos sebességére.
+     */
     public void setSpeed(int speed) {
-        this.speed = speed;
+       Logger.methodEntry(this);
+       
+       this.speed = speed;
+       
+       Logger.methodExit(this);    	
     }
     
 }
